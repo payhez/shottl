1dc5ffdf74a24b4b5d969daaf44272da197542a4
payhez <payhez@gmail.com>
2024-10-27 21:30:24 +0100

    cdc

--- src/main/java/com/shuttler/controller/InstitutionSignUpController.java
+++ src/main/java/com/shuttler/controller/InstitutionSignUpController.java
@@ -5,13 +5,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpResponse;
-
 @RestController
 public class InstitutionSignUpController {
 
     @PostMapping("")
-    ResponseEntity<?> signUpInstitution(@RequestBody Institution institutionRequest) {
+    ResponseEntity<?> signUpInstitution(@RequestBody institutionRequest) {
 
         return null;
     }
