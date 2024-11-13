824c224b03c33c349083ee880ff31afeae5f4a61
payhez <payhez@gmail.com>
2024-11-13 14:07:35 +0100

    app connected to db

--- src/main/java/com/shuttler/ShuttlerApplication.java
+++ src/main/java/com/shuttler/ShuttlerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class ShuttlerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShuttlerApplication.class, args);
