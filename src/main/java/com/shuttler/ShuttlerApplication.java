2cc0ff103625b1aca9934bb318ebef44c962a610
payhez <payhez@gmail.com>
2024-12-28 22:54:38 +0100

    line

--- src/main/java/com/shuttler/ShuttlerApplication.java
+++ src/main/java/com/shuttler/ShuttlerApplication.java
@@ -8,5 +8,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class ShuttlerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShuttlerApplication.class, args);
+
 	}
 }
