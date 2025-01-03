51f09504cc7fcec384b7cd074ae2166f12e42569
payhez <payhez@gmail.com>
2025-01-03 20:25:32 +0300

    integration test

--- src/test/java/com/shuttler/TestShuttlerApplication.java
+++ src/test/java/com/shuttler/TestShuttlerApplication.java
@@ -3,9 +3,7 @@ package com.shuttler;
 import org.springframework.boot.SpringApplication;
 
 public class TestShuttlerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.from(ShuttlerApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
-
 }
