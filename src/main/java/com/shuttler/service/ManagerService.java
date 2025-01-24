10f404c0e8aba8a813aea76217a2cdc9894acc97
payhez <payhez@gmail.com>
2025-01-24 11:29:46 +0300

    update

--- src/main/java/com/shuttler/service/ManagerService.java
+++ src/main/java/com/shuttler/service/ManagerService.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
