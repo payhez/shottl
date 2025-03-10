403a6c8d1dfbe148b7df200fa9d95e5cf1ba83b0
payhez <payhez@gmail.com>
2025-03-10 19:52:36 +0100

    add integration test

--- src/test/java/com/shuttler/controller/OrganisationAdditionIT.java
+++ src/test/java/com/shuttler/controller/OrganisationAdditionIT.java
@@ -5,18 +5,26 @@ import com.shuttler.BaseTest;
 import com.shuttler.controller.request.AddOrganisationRequest;
 import com.shuttler.dao.OrganisationRepository;
 import com.shuttler.model.Address;
-import com.shuttler.model.Organisation;
 import com.shuttler.model.enums.OrganisationType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.geo.Point;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import reactor.test.StepVerifier;
 
+import java.util.Objects;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class OrganisationAdditionIT extends BaseTest {
 
     @Autowired
@@ -27,6 +35,8 @@ public class OrganisationAdditionIT extends BaseTest {
 
     @Autowired
     private OrganisationRepository organisationRepository;
+
+    private static final String URL_TEMPLATE = "/api/v1/organisation/add";
 
     @Test
     public void testOrganisationSignUp() throws Exception {
@@ -42,15 +52,17 @@ public class OrganisationAdditionIT extends BaseTest {
                 .geoLocation(new Point(25.5, 78.9))
                 .organisationType(OrganisationType.SCHOOL).build();
 
-        String responseAsString = mockMvc.perform(post("/signup/organisation")
+        ResponseEntity<?> response = (ResponseEntity<?>) mockMvc.perform(post(URL_TEMPLATE)
+                        .with(jwt().jwt(jwt -> jwt.subject("test-user")))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().is2xxSuccessful())
-                .andReturn().getResponse().getContentAsString();
-        Organisation savedOrganisation = objectMapper.readValue(responseAsString, Organisation.class);
+                .andReturn().getAsyncResult(5000);
 
-        StepVerifier.create(organisationRepository.findById(savedOrganisation.getId()))
-                .expectNextMatches(savedEntity -> savedEntity.getOrganisationName().equals("Test Organisation"))
+        Assertions.assertEquals("Organisation saved successfully.", response.getBody());
+
+        StepVerifier.create(organisationRepository.findByOrganisationName(request.getName()))
+                .expectNextMatches(Objects::nonNull)
                 .expectComplete()
                 .verify();
     }
